79c77bd30c9ee8a0d7b1eaa58fa8abe4f9a8679f
njj3400 <127723818+njj3400@users.noreply.github.com>
2023-03-18 21:32:09 +0800

    Add files via upload
    clean up all tables

---
+++ UClean.sql
@@ -0,0 +1,12 @@
+/*******************
+
+  Cleaning script
+
+*******************/
+
+DROP TABLE IF EXISTS reports;
+DROP TABLE IF EXISTS loans;
+DROP TABLE IF EXISTS umbrellas;
+DROP TABLE IF EXISTS stations;
+DROP TABLE IF EXISTS users;
+
