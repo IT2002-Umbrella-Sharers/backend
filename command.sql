c0254cc0883ce1c48c4d1b2e0215759e383ba264
njj3400 <127723818+njj3400@users.noreply.github.com>
2023-03-21 23:54:06 +0800

    Update command.sql
    all example commands done

--- command.sql
+++ command.sql
@@ -20,26 +20,74 @@ UPDATE users
 SET balance=balance-200
 WHERE email_address = 'PLACEHOLDER@hotmail.com';
 --What umbrellas user is currently borrowing--
-SELECT l.start_date,u.colour,u.size,s.name
+SELECT l.start_date,u.colour,u.size,s.name --Can we just use umbrella_id and start_date as a primary key?
 FROM loans l, umbrellas u,stations s
 WHERE l.borrower = 'PLACEHOLDER@hotmail.com' AND l.umbrella_id = u.id 
 AND u.location = s.id AND l.end_date ISNULL;
 --Current status of user's umbrellas--
+SELECT u.colour, u.size, s.name
+FROM umbrellas u, stations s
+WHERE u.location = s.id AND u.owner = 'PLACEHOLDER@hotmail.com';
 --Return Umbrella--
+SELECT l.umbrella_id, l.borrower, u.owner
+FROM loans l, umbrellas u
+WHERE id = 1 AND l.umbrella_id = u.id; --collect info
+
+UPDATE umbrellas --set umbrella location
+SET location = 1
+WHERE id = loans.umbrella_id;
+
+UPDATE loans  --set loan end date--
+SET end_date = '2023-01-01 01:23:45';
+
+SELECT EXTRACT(DAY from end_date-start_date)+1 as days --number of days inclusive since borrowing
+FROM loans
+WHERE id = 1;
+
+UPDATE users --subtract fees
+SET balance = balance-days
+WHERE email_address = borrower;
+
+UPDATE users --add credit
+SET balance = balance + days*0.7
+WHERE email_address = owner;
 --Submit Report--
+INSERT INTO reports (umbrella_id, reporter, details, date)
+VALUES (1,'PLACEHOLDER@hotmail.com', 'umbrella broke', '2023-01-01 01:23:45');
 /*******************
   Loan an Umbrella
 ********************/
---Select Location--
 --Insert Umbrella Details--
+INSERT INTO umbrellas (colour, size, owner, location)
+VALUES ('#000000', 1, 'PLACEHOLDER@hotmail.com',1)
 /*******************
   Borrow an Umbrella
 ********************/
---Select a Location--
 --Pick an Umbrella--
+SELECT id
+FROM umbrellas
+WHERE location = 1
+AND id NOT IN(
+	SELECT umbrella_id
+	FROM loans
+	WHERE end_date ISNULL);
+INSERT INTO loans (umbrella_id, borrower, start_date, end_date)
+VALUES (1, 'PLACEHOLDER@hotmail.com','2023-01-01 01:23:45',null); --can we use default null in schema?
 /*******************
        Admin
 ********************/
 --Look at Reports--
+SELECT * 
+FROM reports
+WHERE
 --Ban Users--
+UPDATE users
+SET is_banned = TRUE
+WHERE email_address = 'PLACEHOLDER@hotmail.com';
 --Update Locations and umbrellas--
+UPDATE locations
+SET name = 'Bukit Merah MRT'
+WHERE name = 'Redhill MRT';
+UPDATE umbrellas
+SET colour = '#123456'
+WHERE id = 1;
